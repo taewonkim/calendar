f7f332055e4f561abf568d6444ddab3c9e2cf4ea
Taewon Kim <mniktw@gmail.com>
2014-02-26 21:40:13 +0900

    Update calendar.c

--- src/calendar.c
+++ src/calendar.c
@@ -177,9 +177,9 @@ long get_passed_days(u8 m, u16 y)
 u8 number_of_week(u8 d, u8 m, u16 y)
 {
 	long days = 0
+	d = validate_number_u8(d);
 	m = validate_number_u8(m);
 	y = validate_number_u16(y);
-	d = validate_number_u8(d);
 	days  = get_passed_days(m, y);
 	days += d;
 	return (days % 7);
