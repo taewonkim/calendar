bb2dc8c48454d868da8bda4b2b8b0d2dfedba344
Taewon Kim <mniktw@gmail.com>
2014-02-26 21:18:47 +0900

    Update calendar.h

--- src/calendar.h
+++ src/calendar.h
@@ -1,6 +1,11 @@
 #ifndef __H_CALENDAR__
 #define __H_CALENDAR__
 
+#define WEEK_OF_SIZE              3
+#define MAX_MONTH_COUNT          12
+#define DAYS_NORM_MONTH_TWO      28
+#define DAYS_LEAP_MONTH_TWO      29
+
 void _set_days_in_month(u8, u8);
 u8 _get_days_in_month(u8);
 u8 number_of_week(u8, u8, u16);
